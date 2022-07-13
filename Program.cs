f767c9f209a6e83c9d27c0e1939a44fb76c41cbc
Emily Rowley <erowley49@gmail.com>
2022-07-13 14:58:46 -0600

    finished!

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ namespace Unit05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Player 1: top game, right hole, pockets #'s are left to right. \nPlayer 2: bottom pockets, left hole. Pockets numbered right to left.");
-            
             Director director = new Director();
             director.runGame();
         }
